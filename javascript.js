095eb9fc849cf636f05608bb7b9519166ed5281e
fieryone1 <129227697+fieryone1@users.noreply.github.com>
2023-12-18 18:17:11 +0000

    Added GUI and lives logic for game

--- javascript.js
+++ javascript.js
@@ -1,64 +1,106 @@
+const btnrock = document.querySelector("#rock");
+const btnpaper = document.querySelector("#paper");
+const btnscissors = document.querySelector("#scissors");
+const textbox = document.querySelector("#textbox");
+const lifedisplay = document.querySelector("#lives");
+
+let lives;
+let complives;
 
 
+
+function main(){
+lifedisplay.textContent=""
+lives = 5;
+complives=5;
 function compchoice() {
-    let choices=['rock','paper','scissors'];
-    let ran=Math.floor(Math.random()*3);
-    return(choices[ran]);
-}
-
-function playerchoice(){
-    let choice=prompt("Choose rock paper or scissors");
-    while(choice !='rock'.toLowerCase() && choice!='paper'.toLowerCase() && choice!='scissors'.toLowerCase()){
-        choice=prompt("choose rock paper or scissors correctly")
-        
-    }
-    return(choice.toLowerCase());
-
-}
-
-function decidewinner(computer,player) {
-    if (computer==player){
-        return('it is a draw')
-    }
-    else if (computer=='rock'){
-        if (player=='paper'){
-            return('player wins')
-        }
-        else{
-            return('computer wins')
-        }
-    }
-    else if(computer=='paper'){
-        if(player=='scissors'){
-            return('player wins')
-        }
-        else{
-            return('computer wins')
-        }
-
-    }
-    else{
-        if(player=='rock'){
-            return('player wins')
-        }
-        else{
-            return('computer wins' )
-        }
-
-    }
-
-
+  let choices = ["rock", "paper", "scissors"];
+  let ran = Math.floor(Math.random() * 3);
+  return choices[ran];
 }
 
 
-function game(){
-   let player=playerchoice()
-   let computer=compchoice()
 
-   console.log('Player chose ', player," computer chose ", computer, " so ",decidewinner(computer,player))
+function decidewinner(computer, player) {
+  if (computer == player) {
+    return "draw";
+  } else if (computer == "rock") {
+    if (player == "paper") {
+      return "player";
+    } else {
+      return "computer";
+    }
+  } else if (computer == "paper") {
+    if (player == "scissors") {
+      return "player";
+    } else {
+      return "computer";
+    }
+  } else {
+    if (player == "rock") {
+      return "player";
+    } else {
+      return "computer";
+    }
+  }
+}
+
+function game(player) {
+
+
+
+
+
+
+  computer = compchoice();
+  winner = decidewinner(computer, player);
+  if (winner=='draw') {
+    output = `Both players chose ${player} so it is a draw`;
+  }
+  else{
+  output = `The player chose ${player} and the computer chose ${computer} so ${winner} wins this round`;
+  if(winner=='computer'){
+    lives=lives-1;
+  }
+  else{
+    complives=complives-1
+
+  }
+ }
     
 
 
+  lifedisplay.textContent = `player lives: ${lives} computer lives ${complives}`;
+  textbox.textContent = output;
+
+
+  if (lives==0){
+    textbox.textContent='Player lost new game loaded'
+    lives=5
+    complives=5
+    
+}
+else if (complives==0){
+    textbox.textContent='Congrats you win a new game has loaded'
+    lives=5
+    complives=5
+    
 }
 
-game()
+
+
+}
+
+btnrock.addEventListener("click", function () {
+  game("rock");
+});
+btnpaper.addEventListener("click", function () {
+  game("paper");
+});
+btnscissors.addEventListener("click", function () {
+  game("scissors");
+});
+
+}
+
+main()
